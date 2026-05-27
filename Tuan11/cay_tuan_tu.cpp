0664785151e49bf38559aa96377551fe1b8349f4
vudangkhai11 <vudangkhai11@gmail.com>
2026-05-27 09:13:14 +0700

    thêm hàm heapify

--- Tuan11/cay_tuan_tu.cpp
+++ Tuan11/cay_tuan_tu.cpp
@@ -1,21 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void heapify(int a[], int n, int i) {
-    int l = 2 * i + 1;
-    int r = 2 * i + 2;
-    int largest = i;
-    if(l < n && a[l] > a[largest]) {
-        largest = l;
-    }
-    if(r < n && a[r] > a[largest]) {
-        largest = r;
-    }
-    if(largest != i) {
-        swap(a[i], a[largest]);
-        heapify(a, n, largest);
-    }
-}
+
 
 int main() {
 

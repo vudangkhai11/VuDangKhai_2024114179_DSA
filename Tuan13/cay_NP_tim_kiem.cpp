500885cec21e0f98079ea5ee94bb9b301954a832
vudangkhai11 <vudangkhai11@gmail.com>
2026-05-31 20:33:44 +0700

    test ví dụ thành công

--- Tuan13/cay_NP_tim_kiem.cpp
+++ Tuan13/cay_NP_tim_kiem.cpp
@@ -41,7 +41,25 @@ void tim_SV(Node* goc, int x) {
 
 
 int main() {
+    SV ds[] = {
+        {"An", 2001},
+        {"Binh", 2002},
+        {"Cuong", 2006},
+        {"Dung", 2007},
+        {"Ha", 2003},
+        {"Lan", 2004},
+        {"Minh", 2005},
+        {"Nam", 2001},
+        {"Phuong", 1999},
+        {"Trang", 2004}
+    };
 
+    Node* goc = nullptr;
+
+    for(auto sv : ds) goc = them_node_BST(goc, sv);
+
+    cout << "Sinh vien sinh nam 2004: \n";
+    tim_SV(goc, 2004);
     return 0;
 }
 // 2001, 2002, 2006, 2007, 2003, 2004, 2005, 2001, 1999, 2004

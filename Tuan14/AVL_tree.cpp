9cd4b490e0c1be363e8938282a06de5caaa11988
vudangkhai11 <vudangkhai11@gmail.com>
2026-06-08 07:48:52 +0700

    sửa lỗi hàm quay_phai và quay_trai

--- Tuan14/AVL_tree.cpp
+++ Tuan14/AVL_tree.cpp
@@ -24,11 +24,20 @@ Node* quay_phai(Node* y) {
     Node* T2 = x->right;
     x->right = y;
     y->left = T2;
-    y->height = max(height(y->left), height(y->right)) + 1;
-    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(chieu_cao(y->left), chieu_cao(y->right)) + 1;
+    x->height = max(chieu_cao(x->left), chieu_cao(x->right)) + 1;
     return x;
 }
 
+Node* quay_trai(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+    y->left = x;
+    x->right = T2;
+    x->height = max(chieu_cao(x->left), chieu_cao(x->right)) + 1;
+    y->height = max(chieu_cao(y->left), chieu_cao(y->right)) + 1;
+    return y;
+}
 
 
 int main() {

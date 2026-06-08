f5993b2da260211643aea57c7c341c2ad8d7b766
vudangkhai11 <vudangkhai11@gmail.com>
2026-06-08 07:46:13 +0700

    thêm hàm quay_phai

--- Tuan14/AVL_tree.cpp
+++ Tuan14/AVL_tree.cpp
@@ -2,9 +2,34 @@
 using namespace std;
 
 struct Node{
-    int data;
+    int key;
     Node *left, *right;
+    int height;
 };
+
+int chieu_cao(Node* n) {
+    return n ? n->height : 0;
+}
+
+Node* new_Node(int key) {
+    Node* node = new Node();
+    node->key = key;
+    node->left = node->right = nullptr;
+    node->height = 1;
+    return node;
+}
+
+Node* quay_phai(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+    x->right = y;
+    y->left = T2;
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+    return x;
+}
+
+
 
 int main() {
 

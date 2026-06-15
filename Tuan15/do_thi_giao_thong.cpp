2ccc125d25f0cb7bb3d37bb8b1279819b0067e57
vudangkhai11 <vudangkhai11@gmail.com>
2026-06-15 23:02:59 +0700

    khai báo 11 thành phố

--- Tuan15/do_thi_giao_thong.cpp
+++ Tuan15/do_thi_giao_thong.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 using namespace std;
 
+const int N = 11;
 
+string city[N] =
+{
+    "Ha Noi",
+    "Son Tay",
+    "Hoa Binh",
+    "Phu Ly",
+    "Hung Yen",
+    "Hai Duong",
+    "Hai Phong",
+    "Uong Bi",
+    "Bac Giang",
+    "Bac Ninh",
+    "Thai Nguyen"
+};
 
 int main() {
 

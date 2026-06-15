b5c01c93da77bb13f2e3236d3d99b25ca8171f58
vudangkhai11 <vudangkhai11@gmail.com>
2026-06-15 23:13:15 +0700

    chạy test thành công

--- Tuan15/do_thi_giao_thong.cpp
+++ Tuan15/do_thi_giao_thong.cpp
@@ -25,6 +25,36 @@ void them_Canh(int a[][N], int u, int v)
 }
 
 int main() {
+        int a[N][N] = {0};
+
+    them_Canh(a,0,1);
+    them_Canh(a,0,2);
+    them_Canh(a,0,3);
+    them_Canh(a,0,5);
+    them_Canh(a,0,9);
+    them_Canh(a,0,10);
+
+    them_Canh(a,3,4);
+    them_Canh(a,4,5);
+
+    them_Canh(a,5,6);
+
+    them_Canh(a,6,7);
+
+    them_Canh(a,7,8);
+    them_Canh(a,8,9);
+
+    them_Canh(a,9,7);
+
+    cout << "Ma tran ke:\n";
+
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+            cout << a[i][j] << " ";
+
+        cout << endl;
+    }
 
     return 0;
 }

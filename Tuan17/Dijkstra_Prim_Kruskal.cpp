4ecf42ac5fc1692f5a00e733606ae954fae7d420
vudangkhai11 <vudangkhai11@gmail.com>
2026-06-30 23:27:07 +0700

    khởi tạo đồ thị

--- Tuan17/Dijkstra_Prim_Kruskal.cpp
+++ Tuan17/Dijkstra_Prim_Kruskal.cpp
@@ -6,6 +6,15 @@ const int kocoduongdi = 1000000000;
 string tentp[11] = {  "Ha Noi", "Hai Duong", "Hung Yen", "Phu Ly", "Hoa Binh", "Son Tay", "Thai Nguyen", "Bac Ninh", "Bac Giang", "Uong Bi", "Hai Phong"};
 
 int matran[11][11];
+
+void taodothi() {
+    for (int i = 0; i < 11; i++) {
+        for (int j= 0; j < 11; j++) {
+            if (i== j) matran[i][j] = 0;
+            else matran[i][j] = kocoduongdi;
+        }
+    }
+}
 int main() {
 
     return 0;

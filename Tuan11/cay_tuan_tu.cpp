2d6cb92672c89a051f946909cf7244c6a06c7d22
vudangkhai11 <vudangkhai11@gmail.com>
2026-06-30 23:44:23 +0700

    khai báo thuộc tính cây

--- Tuan11/cay_tuan_tu.cpp
+++ Tuan11/cay_tuan_tu.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
+#include <cmath>
 using namespace std;
 
-
-
+class CayTuanTu {
+private:
+    char* cay;          // Mảng động lưu cấu trúc cây
+    int kichThuoc;      // Kích thước hiện tại của mảng
+    char TRONG = '-';   // Ký tự đại diện cho nút trống
+};
 int main() {
 
     return 0;

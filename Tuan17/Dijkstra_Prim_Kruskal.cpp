2b7a2191bccdfceb422dbdceedf4197134e766da
vudangkhai11 <vudangkhai11@gmail.com>
2026-06-30 23:26:07 +0700

    khai báo tham số

---
+++ Tuan17/Dijkstra_Prim_Kruskal.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+const int kocoduongdi = 1000000000;
+
+string tentp[11] = {  "Ha Noi", "Hai Duong", "Hung Yen", "Phu Ly", "Hoa Binh", "Son Tay", "Thai Nguyen", "Bac Ninh", "Bac Giang", "Uong Bi", "Hai Phong"};
+
+int matran[11][11];
+int main() {
+
+    return 0;
+}
